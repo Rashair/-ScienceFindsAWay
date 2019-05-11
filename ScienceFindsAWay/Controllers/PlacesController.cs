d08f6624a859a1170bb02c2189ca918cbcaa4ae8
Piotr Lewandowski <lewandowskipiotr98@gmail.com>
2019-05-11 17:03:37 +0200

    Model and Controller template for Places

---
+++ ScienceFindsAWay/Controllers/PlacesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScienceFindsAWay.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlacesController : Controller
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<Place> GetAllPlaces()
+        {
+            
+        }
+    }
+}
---
+++ ScienceFindsAWay/Models/Place.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScienceFindsAWay.Models
+{
+    public class Place
+    {
+        public int index;
+        public string name;
+        public string address;
+        public string description;
+    }
+}
