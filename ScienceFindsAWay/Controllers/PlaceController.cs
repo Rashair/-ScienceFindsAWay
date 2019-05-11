c2399b4a6bfaffe971d6148d49a937cda99359d3
Piotr Lewandowski <lewandowskipiotr98@gmail.com>
2019-05-11 17:10:02 +0200

    Refactored PlaceController

--- ScienceFindsAWay/Controllers/PlaceController.cs
+++ ScienceFindsAWay/Controllers/PlaceController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ScienceFindsAWay.Models;
 
 namespace ScienceFindsAWay.Controllers
 {
     [Route("api/[controller]")]
-    public class PlacesController : Controller
+    public class PlaceController : Controller
     {
         [HttpGet("[action]")]
         public IEnumerable<Place> GetAllPlaces()
         {
-            
+            return null;
         }
     }
 }
