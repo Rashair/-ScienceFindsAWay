8be2c103cd31c250c4f48e806894c500c33ec426
michalMilewski-8 <miles98@interia.pl>
2019-05-11 17:15:46 +0200

    Added UserControler Made Users

--- ScienceFindsAWay/Models/User.cs
+++ ScienceFindsAWay/Models/User.cs
@@ -7,6 +7,28 @@ namespace ScienceFindsAWay.Models
 {
     public class User
     {
+        public string Name { get; protected set; }
+        public string Surname { get; protected set; }
+        public string University { get; protected set; }
+        public string Faculty { get; protected set; }
+        public string Mail { get; protected set; }
+        public int UserID { get; }
+        private string _password;
 
+        public User(string n, string s, string u, string f,string m, int uid, string pass)
+        {
+            Name = n;
+            Surname = s;
+            University = u;
+            Faculty = f;
+            Mail = m;
+            UserID = uid;
+            _password = pass;
+        }
+
+        public bool CheckPassword(string pass)
+        {
+            return pass == _password;
+        }
     }
 }
