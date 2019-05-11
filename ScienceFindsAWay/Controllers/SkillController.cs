0b4cc81d97abdd1778bfb4760b309962a7597cf3
michalMilewski-8 <miles98@interia.pl>
2019-05-11 18:49:28 +0200

    added skill and category model SkillControler createf

---
+++ ScienceFindsAWay/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ScienceFindsAWay.Models;
+
+namespace ScienceFindsAWay.Controllers
+{
+    [Route("api/[controller]")]
+    public class SkillController : Controller
+    {
+        
+    }
+}
---
+++ ScienceFindsAWay/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScienceFindsAWay.Models
+{
+    public class Category
+    {
+        public Category(int categoryID, string name, int categoryLevel)
+        {
+            CategoryID = categoryID;
+            Name = name;
+            CategoryLevel = categoryLevel;
+        }
+
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public int CategoryLevel { get; set; }
+
+
+    }
+}
