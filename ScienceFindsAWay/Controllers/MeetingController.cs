3285f50a6dca5faa34f5aa3d9904aa121cf6a2a0
michalMilewski-8 <miles98@interia.pl>
2019-05-11 21:05:05 +0200

    fixing MeetingControler part1/2

--- ScienceFindsAWay/Controllers/MeetingController.cs
+++ ScienceFindsAWay/Controllers/MeetingController.cs
@@ -44,7 +44,7 @@ namespace ScienceFindsAWay.Controllers
                             var categories = categoryControler.GetCategoriesByMeetingId(id);
                             var participants = userControler.GetUsersByMeetingId(id);
 
-                            userList.Add(new Meeting(id,name,date,place,categories,participants));
+                            userList.Add(new Meeting(id,name,date,place,categories,participants.ToList()));
                         }
                     }
                 }
