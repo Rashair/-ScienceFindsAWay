ab12d948223f7d6b19f2d53b77b50320a49f4626
Mateusz Majewski <metiulekm@gmail.com>
2019-05-11 16:40:18 +0200

    USOS attempt

---
+++ ScienceFindsAWay/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Authentication;
+
+[Route("api/[controller]/[action]")]
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Login(string returnUrl = "/")
+    {
+        return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+    }
+}
--- ScienceFindsAWay/Startup.cs
+++ ScienceFindsAWay/Startup.cs
@@ -6,6 +6,17 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+using Newtonsoft.Json.Linq;
+
 namespace ScienceFindsAWay
 {
     public class Startup
@@ -21,6 +32,45 @@ namespace ScienceFindsAWay
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "USOS";
+            })
+            .AddCookie()
+            .AddOAuth("USOS", options =>
+            {
+                options.ClientId = Configuration["USOS:ClientId"];
+                options.ClientSecret = Configuration["USOS:ClientSecret"];
+                options.CallbackPath = new PathString("/signin");
+
+                options.AuthorizationEndpoint = "https://apps.usos.pw.edu.pl/services/oauth/authorize";
+                options.TokenEndpoint = "https://apps.usos.pw.edu.pl/services/oauth/access_token";
+                options.UserInformationEndpoint = "https://apps.usos.pw.edu.pl/services/users/user";
+
+                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                options.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "first_name");
+                options.ClaimActions.MapJsonKey(ClaimTypes.Surname, "last_name");
+
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        context.RunClaimActions(user);
+                    }
+                };
+            });
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -46,6 +96,8 @@ namespace ScienceFindsAWay
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
