0983f203edac0385339b48d28df7f5f4b58e8162
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-05-12 01:31:01 +0200

    Added table component and loading

--- ScienceFindsAWay/ClientApp/src/components/Meetings.js
+++ ScienceFindsAWay/ClientApp/src/components/Meetings.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import MeetingsTable from './MeetingsTable'
 
 class Meetings extends Component {
     constructor(props) {
       super(props);
   
       this.state = {
-        Meetings: []
+        Meetings: [],
+        isLoading: false,
       };
     }
   
     componentDidMount() {
-      // This method is called when the component is first added to the document
+      this.setState({
+        isLoading:true,
+      });
+
       fetch(`api/meeting/getAllMeetings`)
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
-            Meetings: result
+            Meetings: result,
+            isLoading: false,
           });
         }
       )
@@ -24,29 +30,17 @@ class Meetings extends Component {
   
   
     render() {
+      if(this.state.isLoading)
+      {
+        return (
+          <img alt="Loading..." src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+        )
+      }
+
       return (
         <div>
           <h1>Meeting list</h1>
-          <table id="Meetings" className="table table-responsive">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Date</th>
-                <th>Place</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.Meetings.map((Meeting, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{Meeting.name}</td>
-                    <td>{Meeting.date}</td>
-                    <td>{Meeting.place.name}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <MeetingsTable table={this.state.Meetings} />
         </div>
       );
     }
---
+++ ScienceFindsAWay/ClientApp/src/components/MeetingsTable.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class MeetingsTable extends Component {
+    render() {
+        this.items = this.props.table.map((Meeting, i) => {
+            var date = new Date(Meeting.date);
+            return (
+                <tr key={i}>
+                    <td>{Meeting.name}</td>
+                    <td>
+                        {date.toLocaleDateString()} {date.toLocaleTimeString().slice(0, 5)}
+                    </td>
+                    <td>{Meeting.place.name}</td>
+                </tr> 
+            );}        
+        );
+
+        return (
+            <table id="Meetings" className="table table-responsive">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Place</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.items}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default MeetingsTable;
